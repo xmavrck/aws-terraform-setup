fedc4efe7f0eb289925ec2d6bab5771938bfa764
Data Maverick <navdeep@xenonstack.com>
2019-07-31 01:06:40 +0530

    update mongo ready status check

--- k8s-deploy/deploy_service.sh
+++ k8s-deploy/deploy_service.sh
@@ -11,9 +11,8 @@ if [ "${Deploy_First_Time_Services}" = 'true' ]; then {
 i=`openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc  get sts -nhero-flow mongodb-replicaset -o jsonpath="{.status.readyReplicas}"  --kubeconfig ./config`
 while [ "$i" != "1" ] 
 do
-    i=`openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc get sts -nhero-flow mongodb-replicaset  --kubeconfig ./config | awk 'FNR>1 {print $3}'`
     echo "Mongo DB is creating"
-    echo $i
+    i=`openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc  get sts -nhero-flow mongodb-replicaset -o jsonpath="{.status.readyReplicas}"  --kubeconfig ./config`
 done
 openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc apply -f k8s-deploy/db-setup.yaml -n hero-flow  --kubeconfig ./config 
 openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc create secret docker-registry saleshero-registry-secret --docker-username=rzwodzwo --docker-password=8YTOqtGswl@h0vJs2wrgyGX7 --docker-email=r2d2@automationhero.ai -nhero-flow  --kubeconfig ./config 
